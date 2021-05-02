9b17ca1fb43a77ef077061ab9fbcb3521b6f0c66
straightupjac <16831223+straightupjac@users.noreply.github.com>
2021-05-02 21:43:59 +0800

    video

--- src/components/creative/Choreography.vue
+++ src/components/creative/Choreography.vue
@@ -1,17 +1,71 @@
 <template>
   <div>
     <v-container>
-      <h2>Choreography</h2>
+    <v-row align="center"
+      justify="center">
+      <v-col md=10>
+        <h2>Choreography</h2>
+      </v-col>
+      <v-spacer/>
+      <v-btn
+        tile @click=" $router.push('/creative')"
+        elevation=0 class="mt-4 site-btn">
+        Back
+      </v-btn>
+    </v-row>
+    <v-row>
+      <v-col>
+        <Video
+          :videoID="'HLCXgYaAab0'"
+          :title="'Momentum'"
+          :date="'April 2018'"
+          :description="'Momentum abstractly explores the concept of elastic and inelastic collisions surrounding momentum. It presents an unstructured narrative of particles colliding and transferring energy. Four dancers are in a state of constant connection.'"
+          />
+      </v-col>
+      <v-col>
+        <Video
+          :videoID="'vkoBT9hsJmA'"
+          :title="'Golden Ticket'"
+          :date="'November 2017'"
+          :description="'Violet Beauregarde receives her golden ticket in Charlie and the Chocolate Factory.'"
+          />
+      </v-col>
+      <v-col>
+        <Video
+          :videoID="'1GYPBj0vc6w'"
+          :title="'(De)vice'"
+          :date="'March 2017'"
+          :description="'A choreographic collaboration with ten dancers/choreographers. Exploring the impact that digital media and technology plays in our lives.'"
+        />
+      </v-col>
+       <v-col>
+        <Video
+          :videoID="'vCpoYcK7rS0'"
+          :title="'Surrender'"
+          :date="'November 2016'"
+          :description="'A feeling of constantly being pulled in different directions.'"
+        />
+      </v-col>
+      <v-col>
+        <Video
+          :videoID="'K857RmOtUcg'"
+          :title="'Mindheist'"
+          :date="'March 2018'"
+          :description="'A choreographic collaboration with ten dancers/choreographers. Exploration of alzheimer\'s disease. A progressive, irreversible mental condition characterized by loss of memory, agitation and lack of mental stability.'"
+        />
+      </v-col>
+    </v-row>
     </v-container>
   </div>
 </template>
 
 <script>
+import Video from "./Video.vue";
+
 export default {
   name: "Choreography",
+  components: {
+    Video,
+  }
 }
 </script>
-
-<style>
-
-</style>
--- src/components/creative/Performances.vue
+++ src/components/creative/Performances.vue
@@ -1,14 +1,64 @@
 <template>
   <div>
     <v-container>
-      <h2>Performances</h2>
+    <v-row align="center"
+      justify="center">
+      <v-col md=10>
+        <h2>Performances</h2>
+      </v-col>
+      <v-spacer/>
+      <v-btn
+        tile @click=" $router.push('/creative')"
+        elevation=0 class="mt-4 site-btn">
+        Back
+      </v-btn>
+    </v-row>
+    <v-row>
+      <v-col>
+        <Video
+          :videoID="'LU5LKVM0M4g'"
+          :title="'Fusion'"
+          :date="'April 2018'"
+          :description="'Choreographed by Elena Cherry. Performed as part of the full-length production Definition at the Singapore American School'"
+          />
+      </v-col>
+      <v-col>
+        <Video
+          :videoID="'d5A3hqRvqsg'"
+          :title="'The Birds pt 1'"
+          :date="'April 2017'"
+          :description="'Choreographed by Hannah Bradshaw. Performed as part of the full-length production Showcase at the Singapore American School.'"
+          />
+      </v-col>
+       <v-col>
+        <Video
+          :videoID="'nyG2jY05y9U'"
+          :title="'We have it all'"
+          :date="'April 2017'"
+          :description="'Choreographed by Diya Vuthandam. Performed as part of the full-length production Showcase at the Singapore American School.'"
+        />
+      </v-col>
+      <v-col>
+        <Video
+          :videoID="'82zRzwBBsK4'"
+          :title="'Victory'"
+          :date="'April 2017'"
+          :description="'Choreographed by Lucia Garcia Velasco. Performed as part of the full-length production Showcase at the Singapore American School.'"
+        />
+      </v-col>
+    </v-row>
     </v-container>
   </div>
 </template>
 
 <script>
+import Video from "./Video.vue";
+
 export default {
   name: "Performances",
+  components: {
+    Video,
+  }
 }
 </script>
 
