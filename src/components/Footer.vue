057565e70af202008c88a933ab646a90ba968153
straightupjac <16831223+straightupjac@users.noreply.github.com>
2021-07-15 19:15:59 +0800

    add medium account

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -26,6 +26,14 @@
             {{ icon.icon }}
           </v-icon>
         </v-btn>
+        <v-btn
+          href="https://medium.com/@straightupjac"
+          target="_blank"
+          class="mx-4"
+          icon
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M4.37 7.3a.75.75 0 0 0-.25-.65L2.25 4.4v-.34h5.8l4.48 9.83l3.94-9.83H22v.34l-1.6 1.53c-.13.1-.2.28-.17.45v11.24c-.03.17.04.35.17.45l1.56 1.53v.34h-7.84v-.34l1.61-1.57c.16-.15.16-.2.16-.44V8.5L11.4 19.9h-.6L5.57 8.5v7.64c-.07.32.06.64.29.86l2.1 2.57v.33H2v-.33L4.1 17a.94.94 0 0 0 .27-.86V7.3z" fill="#626262"/></svg>
+        </v-btn>
       </v-card-text>
       <v-card-text class="mr-10 ml-10">
         <p>{{ new Date().getFullYear() }} — <a href='http://jaclynchan.me/' target="_blank" style="color:#88b1b1;text-decoration: none;"> Jaclyn Chan</a></p>
@@ -45,7 +53,7 @@ export default {
       {icon: "mdi-twitter", link: "https://twitter.com/straightupjac"},
       {icon: "mdi-github", link: "https://github.com/straightupjac"},
       {icon: "mdi-email", link: "mailto:jdc.jaclyn@gmail.com"},
-      ],
+    ],
   }),
 };
 </script>
